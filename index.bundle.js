54fcd0c2fa0495bc94b2842e4b99ffca5a88c183
lada-nikiforova <lada-nikiforova@users.noreply.github.com>
2024-10-10 11:05:22 +0000

    deploy: 30a9be1afc8e077aa39ae01f69380b293e449d54

--- index.bundle.js
+++ index.bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "50223cece4dc08b2aec0";
+/******/ 	var hotCurrentHash = "2bf36e0cd029694ebcd1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
