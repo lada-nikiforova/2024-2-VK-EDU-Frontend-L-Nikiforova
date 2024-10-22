687f3d134f0fc710aa21d61e05e5838904d7bb47
lada-nikiforova <ladanik2002@gmail.com>
2024-10-22 21:45:55 +0300

    fix reload page

--- react-chat/src/App.jsx
+++ react-chat/src/App.jsx
@@ -1,21 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import PageChat from './pages/PageChat/index.jsx';
 import PageChatList from './pages/PageChatList/index.jsx';
 import {currentPage} from './constant.js';
 
 const App = () => {
-  
-  const [page, setPage] = useState(() => localStorage.getItem(currentPage) || 'chatList');
-
+  const [page, setPage] = useState(() => {const savedPage = localStorage.getItem(currentPage); return savedPage!== null ? savedPage : 'chatList'; });
   const goToChatList = () => {setPage('chatList'); localStorage.setItem(currentPage, 'chatList');};
   const goToChat = () => {setPage('chat'); localStorage.setItem(currentPage, 'chat');};
-
-  useEffect(() => {
-    const savedPage = localStorage.getItem(currentPage);
-    setPage(savedPage);
-  }, []);
-
   return (
     <div>
       {page === 'chat' && <PageChat onBack={goToChatList} />}
