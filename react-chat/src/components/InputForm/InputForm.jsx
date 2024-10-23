3704f4f0fb4a1ce7f9caa47c58cb56700d398da5
lada-nikiforova <ladanik2002@gmail.com>
2024-10-23 11:08:00 +0300

    add trim

--- react-chat/src/components/InputForm/InputForm.jsx
+++ react-chat/src/components/InputForm/InputForm.jsx
@@ -7,6 +7,7 @@ import {activePerson} from '../../constant';
 const InputForm = ({onAddMessage}) => {
     const [message, setMessage] = useState('');
     const [name, setName] = useState(''); 
+    const [error, setError] = useState(false);
     useEffect(()=>{
         const chatData = localStorage.getItem(activePerson);
         setName(chatData);
@@ -14,29 +15,36 @@ const InputForm = ({onAddMessage}) => {
     
     const createMessage = (event) => {
         event.preventDefault(); 
-        
-        const time = new Date();
-        const timeElement = time.getHours().toString().padStart(2, '0') + ':' + time.getMinutes().toString().padStart(2, '0');
-        
-        const newMessage = {
-            id: time,
-            message: message,
-            time: timeElement,
+        if(message.trim().length){
+            const time = new Date();
+            const timeElement = time.getHours().toString().padStart(2, '0') + ':' + time.getMinutes().toString().padStart(2, '0');
             
-        };
-        
-        onAddMessage(newMessage);
-        setTimeout(() => {
-            const newAnswer = {
-                name_a: name,
-                message_a: 'Ответ', 
-                time_a: new Date().getHours().toString().padStart(2, '0') + ':' + new Date().getMinutes().toString().padStart(2, '0')
+            const newMessage = {
+                id: time,
+                message: message,
+                time: timeElement,
+                
             };
             
-            // Обновляем объект сообщения, добавляя ответ
-            onAddMessage({ ...newMessage,  ...newAnswer });
-        }, 1200);
-        setMessage('');
+            onAddMessage(newMessage);
+            setTimeout(() => {
+                const newAnswer = {
+                    name_a: name,
+                    message_a: 'Ответ', 
+                    time_a: new Date().getHours().toString().padStart(2, '0') + ':' + new Date().getMinutes().toString().padStart(2, '0')
+                };
+                
+                // Обновляем объект сообщения, добавляя ответ
+                onAddMessage({ ...newMessage,  ...newAnswer });
+            }, 1200);
+            setMessage('');
+            setError(false);
+
+        }
+        else{
+            setError(true);
+            setMessage('');
+        }
     };
 
 
@@ -44,7 +52,7 @@ const InputForm = ({onAddMessage}) => {
         <div className="lower">
                 <button className="icon-button"> <SentimentSatisfiedIcon sx={{ fontSize: 40 }} className="icon"/></button>
                 <form className="form" action="/" onSubmit={createMessage}>
-                    <input className="form-input" name="message-text" placeholder="Введите сообщение..." type="text" value={message} onChange={(e) => setMessage(e.target.value)} required autoComplete="off"/>
+                    <input className={`form-input ${error ? 'error' : ''}`} name="message-text" placeholder={error ? "Введите корректное сообщение..." : "Введите сообщение..."} type="text" value={message} onChange={(e) => setMessage(e.target.value)} required autoComplete="off"/>
                 </form>
             <button className="send icon-button"><SendIcon sx={{ fontSize: 40 }} className="icon" onClick={createMessage} /></button> 
         </div>
